ff9ecdfd3168cd94a63f8a65eb1e895385e4494d
007 <dr007cc@gmail.com>
2022-02-08 21:42:33 +0330

    useing Category

--- src/App/layout/Categories/index.tsx
+++ src/App/layout/Categories/index.tsx
@@ -1,7 +1,11 @@
 import React, { FC } from 'react'
 
+// redux
+import { CategoryType } from '../../redux/models'
+
 // components
 import Actions from './Actions'
+import Category from './Category'
 
 // style
 import './style/categories.scss'
@@ -9,9 +13,36 @@ import './style/categories.scss'
 const Categories: FC = () => {
     return (
         <div className='categories-container'>
+            <div className='categories'>
+                {CS.map((c, index) => (
+                    <Category {...c} key={index} />
+                ))}
+            </div>
+
             <Actions />
         </div>
     )
 }
 
 export default Categories
+
+const CS: CategoryType[] = [
+    {
+        id: 1,
+        name: 'category 1234567890 1234567890 1234567890 1234567890 ',
+        checked: true,
+        todos: [
+            { id: 1, name: 'todo 1', checked: true },
+            { id: 2, name: 'todo 2', checked: false },
+            { id: 3, name: 'todo 3', checked: true },
+        ],
+    },
+    // {
+    //     name: 'category 2',
+    //     todos: [{ name: 'todo 1' }, { name: 'todo 2' }, { name: 'todo 3' }],
+    // },
+    // {
+    //     name: 'category 3',
+    //     todos: [{ name: 'todo 1' }, { name: 'todo 2' }, { name: 'todo 3' }],
+    // },
+]
