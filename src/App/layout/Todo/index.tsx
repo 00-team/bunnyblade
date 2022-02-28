57ebe9faf7b0cb472976d0b434192e1b171cfc71
007 <dr007cc@gmail.com>
2022-02-28 17:01:42 +0330

    using todo section as board for config showcases

--- src/App/layout/Todo/index.tsx
+++ src/App/layout/Todo/index.tsx
@@ -6,7 +6,7 @@ import './style/todo.scss'
 interface TodoProps {}
 
 const Todo: FC<TodoProps> = () => {
-    return <div className='todo-container'>Todo</div>
+    return <div className='todo-container'>GG</div>
 }
 
 export default Todo
