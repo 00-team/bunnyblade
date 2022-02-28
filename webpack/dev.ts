7059463af4cc47d84627047c72ef2f7c1d5ae7da
007 <dr007cc@gmail.com>
2022-02-28 16:57:12 +0330

    using webpack dev server instead of html file

--- webpack/dev.ts
+++ webpack/dev.ts
@@ -1,12 +1,26 @@
 import { Configuration } from 'webpack'
+import { Configuration as DevConfig } from 'webpack-dev-server'
 
 // Base config
 import Base from './base'
 
-const Config: Configuration = {
+interface Conf extends Configuration {
+    devServer: DevConfig
+}
+
+const Config: Conf = {
     ...Base,
     mode: 'development',
-    watch: true,
+    devServer: {
+        port: 8000,
+        hot: true, // true = full reload
+        historyApiFallback: true,
+        compress: true,
+        client: {
+            logging: 'none',
+            reconnect: 7,
+        },
+    },
 }
 
 export default Config
