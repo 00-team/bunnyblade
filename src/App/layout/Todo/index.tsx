56ed6a87248c7c4ce4814debde373f267548f539
007 <dr007cc@gmail.com>
2022-03-01 14:48:24 +0330

    basic stuff

--- src/App/layout/Todo/index.tsx
+++ src/App/layout/Todo/index.tsx
@@ -6,7 +6,7 @@ import './style/todo.scss'
 interface TodoProps {}
 
 const Todo: FC<TodoProps> = () => {
-    return <div className='todo-container'>GG</div>
+    return <div className='todo-container'>300ms / 200ms linear</div>
 }
 
 export default Todo
