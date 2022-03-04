a798b4970ef511681d5eb6adec96f8d867edf7e1
007 <dr007cc@gmail.com>
2022-03-04 23:22:51 +0330

    adding style for delete dialog template

--- src/App/layout/Categories/Actions/DeleteDialog.tsx
+++ src/App/layout/Categories/Actions/DeleteDialog.tsx
@@ -1,5 +1,8 @@
 import React, { FC } from 'react'
 
+// style
+import './style/delete-dialog.scss'
+
 // hexa
 import Hexabox from '~Hexa/Hexabox'
 
@@ -11,7 +14,12 @@ const DeleteDialog: FC<DeleteDialogProps> = () => {
     const dialog = useDialog()
     console.log(dialog)
 
-    return <Hexabox>gg</Hexabox>
+    return (
+        <Hexabox width={550}>
+            Are you sure you wanna delete this categories???
+            <div className='del-dia'></div>
+        </Hexabox>
+    )
 }
 
 export default DeleteDialog
