915a94353e904a74f237d8b50dca355faef4e007
007 <dr007cc@gmail.com>
2022-03-06 03:25:14 +0330

    adding close button for delete dialog

--- src/App/layout/Categories/Actions/DeleteDialog.tsx
+++ src/App/layout/Categories/Actions/DeleteDialog.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
 
 // style
 import './style/delete-dialog.scss'
@@ -6,18 +6,84 @@ import './style/delete-dialog.scss'
 // hexa
 import Hexabox from '~Hexa/Hexabox'
 
+// dialog
 import { useDialog } from '~comps/dialog'
+
+// utils
+import { C } from '@00-team/utils'
+
+interface HoverState {
+    hovering: boolean
+    onTriangles: boolean
+    close: boolean
+}
+
+const DefaultHState: HoverState = {
+    hovering: false,
+    onTriangles: false,
+    close: false,
+}
 
 interface DeleteDialogProps {}
 
 const DeleteDialog: FC<DeleteDialogProps> = () => {
     const dialog = useDialog()
-    console.log(dialog)
+    const [active, setActive] = useState<HoverState>(DefaultHState)
+    // cpa == close path active
+    const [CPA, setCPA] = useState(false)
+
+    const closeHover = (hovering: boolean) => {
+        setActive(s => {
+            return { ...s, close: hovering }
+        })
+    }
+
+    const closeShow = () =>
+        active.close || (active.hovering && !active.onTriangles)
 
     return (
-        <Hexabox width={550}>
+        <Hexabox
+            width={550}
+            onHover={props => setActive({ ...active, ...props })}
+        >
             Are you sure you wanna delete this categories???
-            <div className='del-dia'></div>
+            <div className='del-dia'>
+                <svg
+                    className={'close' + C(closeShow(), 'active')}
+                    viewBox='0 0 1 1'
+                    onMouseEnter={() => closeHover(true)}
+                    onMouseLeave={() => closeHover(false)}
+                    style={!closeShow() ? { transitionDelay: '300ms' } : {}}
+                >
+                    <path
+                        className='close-bg'
+                        style={
+                            CPA
+                                ? {}
+                                : closeShow()
+                                ? { transitionDelay: '300ms' }
+                                : {}
+                        }
+                        d='M 0 0 L 1 0 L 1 1 Z'
+                        onClick={() => console.log(dialog)}
+                        onMouseDown={() => setCPA(true)}
+                        onMouseUp={() => setTimeout(() => setCPA(false), 300)}
+                    />
+
+                    <path
+                        style={
+                            CPA
+                                ? {}
+                                : closeShow()
+                                ? { transitionDelay: '300ms' }
+                                : {}
+                        }
+                        className='close-sign'
+                        d='M 0.5 0.08 L 0.9 0.48 Z M 0.9 0.08 L 0.5 0.48 Z'
+                        // d='M 0.6875 0.0625 L 0.6875 0.5625 Z M 0.4375 0.3125 L 0.9375 0.3125 Z'
+                    />
+                </svg>
+            </div>
         </Hexabox>
     )
 }
