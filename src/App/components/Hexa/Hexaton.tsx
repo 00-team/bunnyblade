b24fbbc308fb7b2aade4cc1a72a1f3ee6bfe7800
007 <dr007cc@gmail.com>
2022-03-07 12:55:41 +0330

    init Hexaton. a component for buttons

---
+++ src/App/components/Hexa/Hexaton.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from 'react'
+
+interface HexatonProps {}
+
+const Hexaton: FC<HexatonProps> = ({ children }) => {
+    return <button>{children}</button>
+}
+
+export default Hexaton
