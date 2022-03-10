893daaa12b84a42bb106d869af694c46a289b637
007 <dr007cc@gmail.com>
2022-03-11 00:27:52 +0330

    adding the select checkbox for todos

--- src/App/layout/Categories/Todo.tsx
+++ src/App/layout/Categories/Todo.tsx
@@ -5,18 +5,24 @@ import './style/todo.scss'
 
 // redux state
 import { TodoModel } from 'state/models/Todo'
-import { useDispatch } from 'react-redux'
+import { SelectedTypes } from 'state/models/Selected'
+import { useDispatch, useSelector } from 'react-redux'
 import { Update } from 'state/actions/todo'
+import { RootState } from 'state'
 
 // hexa
 import Hexacheck from '~Hexa/Hexacheck'
+import Hexalect from '~Hexa/Hexalect'
+
+// utils
+import { C } from '@00-team/utils'
 
 const TodosWrapper: FC<{ todos: TodoModel[] }> = ({ todos }) => {
     return (
         <div className='todos-wrapper'>
             <ul>
                 {todos.map((todo, index) => (
-                    <TodoItem {...todo} key={index} />
+                    <TodoItem {...todo} key={index} index={0} />
                 ))}
             </ul>
         </div>
@@ -28,14 +34,32 @@ interface ToderState {
     style: CSSProperties
 }
 
-const TodoItem: FC<TodoModel> = props => {
-    const { id, title, checked } = props
+interface TodoItemProps extends TodoModel {
+    index: number
+}
+
+const TodoItem: FC<TodoItemProps> = props => {
+    const { id, title, checked, index } = props
     const dispatch = useDispatch()
+    const SelectedState = useSelector((s: RootState) => s.Selected)
     const [Toder, setToder] = useState<ToderState>({
         hovering: false,
         style: {},
     })
     const [Checked, setChecked] = useState(checked)
+    const [Selected, setSelected] = useState(false)
+
+    useEffect(() => {
+        if (!SelectedState.active) {
+            setTimeout(() => {
+                setSelected(false)
+            }, index * 100)
+            return
+        }
+
+        if (SelectedState.todos.find(t => t.id === id)) setSelected(true)
+        else setSelected(false)
+    }, [SelectedState])
 
     useEffect(() => {
         dispatch(Update({ id: id, checked: Checked }))
@@ -70,7 +94,24 @@ const TodoItem: FC<TodoModel> = props => {
             onMouseEnter={() => setHovering(true)}
             onMouseLeave={() => setHovering(false)}
         >
-            <span>{title}</span>
+            <div
+                className={'select' + C(SelectedState.active, 'show')}
+                style={{ transitionDelay: `${index * 100}ms` }}
+            >
+                <Hexalect
+                    selected={Selected}
+                    onClick={() =>
+                        dispatch({
+                            type: SelectedTypes.TOGGLE_TODO,
+                            payload: props,
+                        })
+                    }
+                />
+            </div>
+
+            <span>
+                {title} - {id}
+            </span>
             <Hexacheck checked={Checked} onClick={() => setChecked(!Checked)} />
 
             <div className='toder-container'>
